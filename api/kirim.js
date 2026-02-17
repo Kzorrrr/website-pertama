63791dde0748bd73a94b2f9d2d437d097f0daad0
Kzorrrr <mahardikabintang288@gmail.com>
2026-02-17 16:52:50 +0700

    Update kirim.js

--- api/kirim.js
+++ api/kirim.js
@@ -1,9 +1,20 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const { nama } = req.body;
 
+    await client.connect();
+    const db = client.db("belajarCloud");
+    const collection = db.collection("users");
+
+    await collection.insertOne({ nama });
+
     res.status(200).json({
-      message: `Halo ${nama}`
+      message: `Halo ${nama}, data kamu sudah disimpan!`
     });
   } else {
     res.status(405).json({ message: "Method not allowed" });
